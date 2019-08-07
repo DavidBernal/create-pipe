3dd4a52a09e5f7519d3520da49998cfdb5535d49
David Bernal <dbl1987@gmail.com>
2019-08-07 19:59:00 +0200

    Add support to params

--- template/bin/main.js
+++ template/bin/main.js
@@ -2,10 +2,13 @@
 const through2 = require('through2');
 const pipe = require('../pipe');
 
+const args = process.argv.slice(2);
+
 const transform = through2((chunk, enc, callback) =>
   pipe(
     chunk,
-    enc
+    enc,
+    ...args
   )
     .then(result => callback(null, result))
     .catch(err => callback(err))
--- template/pipe.js
+++ template/pipe.js
@@ -1,4 +1,4 @@
-module.exports = async function(chunk /* buffer */, encoding) {
+module.exports = async function(chunk /* buffer */, encoding, ...args) {
   // YOUR CODE HERE
   return chunk;
 };
